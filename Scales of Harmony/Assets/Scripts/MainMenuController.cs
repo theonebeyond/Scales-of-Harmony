f8a886d15b1efc588771f86e79ab7e7928fe8808
BEYONDzzj <beyondzzj@me.com>
2024-01-10 09:47:26 -0500

    Bunch of new stuff
    As shown in the Trello board

---
+++ Scales of Harmony/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene"); // Replace "GameScene" with the name of your actual game scene
+    }
+
+    public void Credits()
+    {
+        
+    }
+}
